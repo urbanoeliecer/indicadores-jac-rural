dee42e0a42489eb7df81e323e38174cf0bde635a
urbanoeliecer <urbanoeliecer@gmail.com>
2025-12-25 12:34:30 -0500

    informe grafico de actividades por mes en municipios de un Dpto

---
+++ infPartComparativo.php
@@ -0,0 +1,26 @@
+<?php
+// Grafica, hazla lleva los valores a un máximo de 1 proporcional al máximo que encuentres 
+$data = [
+    ['Betulia',  2511, 5],
+    ['Betulia',  2512, 6],
+    ['Zapatoca', 2511, 5],
+    ['Zapatoca', 2512, 4],
+    ['Lebrija',  2511, 3],
+    ['Lebrija',  2512, 2],
+];
+
+// Máximo
+$max = max(array_column($data, 2));
+
+// Ancho máximo de la barra (en px)
+$maxWidth = 300;
+
+?>
+
+<table border="0" cellpadding="5">
+<?php foreach ($data as $row): 
+    $normalized = $row[2] / $max;           // valor entre 0 y 1
+    $width = $normalized * $maxWidth;       // ancho proporcional
+?>
+<tr>
+
