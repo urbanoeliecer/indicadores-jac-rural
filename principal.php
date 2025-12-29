0d68ad60111c015ec2d0d1f03378f0b47b30f52b
urbanoeliecer <urbanoeliecer@gmail.com>
2025-12-29 13:10:20 -0500

    Definición nueva estructura

--- principal.php
+++ principal.php
@@ -1,7 +1,32 @@
 <?php
+//session_start();
+?>
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+<nav>
+  <ul>
+    <li><a href="index.php">Inicio</a></li>
+    <li><a href="conect.php">Conexión</a></li>
+    <li><a href="funciones.php">Funciones</a></li>
+
+    <?php // if (isset($_SESSION['rol']) && $_SESSION['rol'] === 'admin'): ?>
+      <li>
+        Admin
+        <ul>
+          <li><a href="admin/infCons.php">Inf. Consolidada</a></li>
+        </ul>
+      </li>
+    <?php //endif; ?>
+
+    <?php //if (isset($_SESSION['rol']) && $_SESSION['rol'] === 'coordinador'): ?>
+      <li>
+        Secretario
+        <ul>
+          <li><a href="coordinador/indDep.php">Información Departamental</a></li>
+          <li><a href="coordinador/indMun.php">Información Municipal</a></li>
+          <li><a href="coordinador/indNac.php">Información Nacional</a></li>
+        </ul>
+      </li>
+    <?php //endif; ?>
+  </ul>
+</nav>
 
